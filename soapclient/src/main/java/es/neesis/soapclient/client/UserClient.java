e4a0abf9aced88ff3b277b306c6c59d62aff4a59
JuanPedroGallardo <juanpedro.gallardo@babelgroup.com>
2025-03-28 13:19:29 +0100

    Metodo login añadido

--- soapclient/src/main/java/es/neesis/soapclient/client/UserClient.java
+++ soapclient/src/main/java/es/neesis/soapclient/client/UserClient.java
@@ -2,7 +2,11 @@ package es.neesis.soapclient.client;
 
 import es.neesis.soapclient.ws.user.GetUserRequest;
 import es.neesis.soapclient.ws.user.GetUserResponse;
+import es.neesis.soapclient.ws.user.LoginRequest;
+import es.neesis.soapclient.ws.user.LoginResponse;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+
+import java.util.Base64;
 
 public class UserClient extends WebServiceGatewaySupport {
 
@@ -11,5 +15,14 @@ public class UserClient extends WebServiceGatewaySupport {
         request.setId(id);
 
         return (GetUserResponse) getWebServiceTemplate().marshalSendAndReceive(request);
+    }
+
+    public LoginResponse login(String username, String password) {
+        LoginRequest request = new LoginRequest();
+        request.setUsername(username);
+        String hashPassword = Base64.getEncoder().encodeToString(password.getBytes());
+        request.setPassword(hashPassword);
+
+        return (LoginResponse) getWebServiceTemplate().marshalSendAndReceive(request);
     }
 }
